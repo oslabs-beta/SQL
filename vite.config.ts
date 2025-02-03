2dfd1d922df31d5c16f544c30cde27f1a0f9fa60
Bryan <bryancabanin@gmail.com>
2025-02-03 13:50:19 -0500

    fixed errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // needed to add this in order for front end and back end to work together
-    proxy: {
-      '/': 'http://localhost:3000',
-    },
-  },
+  // server: {
+  //   // needed to add this in order for front end and back end to work together
+  //   proxy: {
+  //     '/': 'http://localhost:3000',
+  //   },
+  // },
 });
