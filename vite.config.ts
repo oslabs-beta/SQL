db6f16b5741dd6ce955204fed06daded86848ee1
Bryan <bryancabanin@gmail.com>
2025-02-03 14:17:31 -0500

    fixed errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Remove comment once we connect our frontend to our backend
   // server: {
   //   // needed to add this in order for front end and back end to work together
   //   proxy: {
