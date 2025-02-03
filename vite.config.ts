0f1bf591e42cd3c46e6d94ffad5523e85dbd0823
Bryan <bryancabanin@gmail.com>
2025-02-03 12:03:57 -0500

    base

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // needed to add this in order for front end and back end to work together
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+  },
+});
